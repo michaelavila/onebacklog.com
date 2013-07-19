ac81cd28e845f28149ab9daa68317cb01e4611e9
Michael Avila <me@michaelavila.com>
2013-07-18 17:02:25 -0700

    Add files from rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnebacklogCom::Application.config.secret_key_base = '5474b6169b6f6f4d5cd5f6b89c540dbd3ec222cc1cb6e0023470367ca66118081e413c3d7d8e666aaac3a49c79dec83dc76bee5126c8b82714eb381c0636a33a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnebacklogCom::Application.config.session_store :cookie_store, key: '_onebacklog_com_session'
