b108dc544699db1c37dabea02f6cf5cfa310f767
Michael Avila <me@michaelavila.com>
2013-07-20 23:17:04 -0700

    Add realistic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,19 @@
 
 User.delete_all
 
-user = User.create :email => 'user@example.com', :password => 'password longer'
+user = User.create :email => 'me@onebacklog.com', :password => 'password longer'
 
 BacklogItem.delete_all
 Comment.delete_all
 
 item = BacklogItem.create(
   :id => 1,
-  :name => "Really important seed item",
-  :description => "Well, if you didn't notice, it's really important.",
-  :status => "in iteration, not started",
-  :project_list => "Really important project",
-  :label_list => "not-real",
+  :name => "Make backlog item viewable",
+  :description => "Each item in the backlog should have a page dedicated to them. On this page you should be able to view the short name, long description, current status, position in backlog, discussions between team members, creator, updater, timestamps, label tags, project tags and whatever else makes sense. Most of these items can be edited from this page. It should also look nice.",
+  :status => "in iteration, started but not finished",
+  :project_list => "OneBacklog",
+  :label_list => "Items",
   :position => 1,
   :creator => user,
 )
-item.comments.create :comment => "Really awesome comment"
+item.comments.create :comment => "We should include task lists"
