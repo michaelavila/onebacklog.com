7867c048dedc50a75f679e739be49eefb9c1eb2f
Michael Avila <me@michaelavila.com>
2013-07-25 18:24:34 -0700

    Add edit backlog item

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,50 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
+BacklogItem.delete_all
+Comment.delete_all
+ChecklistItem.delete_all
+Assignment.delete_all
 
 user = User.create :email => 'me@onebacklog.com', :password => 'password longer'
 worker = User.create :email => 'worker@onebacklog.com', :password => '123456789'
 
-BacklogItem.delete_all
-Comment.delete_all
+# View Backlog Item
 
-item = BacklogItem.create(
+view_item = BacklogItem.create(
   :id => 1,
   :name => "View backlog item",
   :description => "A page should be dedicated to each individual backlog item. On this page you should be able to view the short name, long description, current status, position in backlog, discussions between team members, creator, updater, timestamps, project tags and whatever else makes sense. Most of these items can be edited from this page. It should also look nice.",
+  :status => "in iteration, finished",
+  :project_list => "onebacklog",
+  :position => 1,
+  :creator => user,
+  :updater => user,
+)
+view_item.comments.create :comment => "We should include task lists", :user => user
+view_item.comments.create :comment => "Maybe there should be multiple task lists?", :user => user
+
+completed = ['name', 'position', 'description', 'projects', 'status', 'discussion', 'checklist']
+['name', 'position', 'description', 'projects', 'status', 'discussion', 'checklist', 'workers'].each do |checklist_item|
+  view_item.checklist_items.create :description => checklist_item, :completed => completed.include?(checklist_item)
+end
+
+Assignment.create :user_id => worker.id, :backlog_item => view_item
+
+# Edit Backlog Item
+
+edit_item = BacklogItem.create(
+  :id => 2,
+  :name => "Edit backlog item",
+  :description => "The view backlog item page should double as an edit page",
   :status => "in iteration, started but not finished",
   :project_list => "onebacklog",
   :position => 1,
   :creator => user,
   :updater => user,
 )
-item.comments.create :comment => "We should include task lists", :user => user
-item.comments.create :comment => "Maybe there should be multiple task lists?", :user => user
 
-ChecklistItem.delete_all
-completed = ['name', 'position', 'description', 'projects', 'status', 'discussion', 'checklist']
+completed = []
 ['name', 'position', 'description', 'projects', 'status', 'discussion', 'checklist', 'workers'].each do |checklist_item|
-  item.checklist_items.create :description => checklist_item, :completed => completed.include?(checklist_item)
+  edit_item.checklist_items.create :description => checklist_item, :completed => completed.include?(checklist_item)
 end
-
-Assignment.delete_all
-Assignment.create :user_id => worker.id, :backlog_item => item
