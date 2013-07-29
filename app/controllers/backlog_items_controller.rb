75b77060e53076c522ab7b2f8de6f111b46a40ad
Michael Avila <me@michaelavila.com>
2013-07-28 22:12:34 -0700

    Change updater when changing name, descript, or adding a comment

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -7,14 +7,16 @@ class BacklogItemsController < ApplicationController
 
   def update
     @backlog_item = BacklogItem.find params[:backlog_item_id]
-    @backlog_item.update_attributes(
-      params.require(:backlog_item).permit :description, :name
-    )
+    params[:backlog_item] = params[:backlog_item].merge :updater_id => current_user.id
+    attributes = params.require(:backlog_item).permit :description, :name, :updater_id
+    @backlog_item.update_attributes attributes
     respond_with @backlog_item
   end
 
   def comment
-    comment = BacklogItem.find(params[:backlog_item_id]).comments.new params.require(:comment).permit(:comment)
+    backlog_item = BacklogItem.find(params[:backlog_item_id])
+    backlog_item.update_attributes :updater_id => current_user.id
+    comment = backlog_item.comments.new params.require(:comment).permit(:comment)
     comment.user = User.all.first
     comment.save
     redirect_to backlog_item_path(params[:backlog_item_id])
--- spec/controllers/backlog_items_controller_spec.rb
+++ spec/controllers/backlog_items_controller_spec.rb
@@ -11,4 +11,41 @@ describe BacklogItemsController do
       expect(assigns :backlog_item).to eq(@backlog_item)
     end
   end
+
+  describe '#update' do
+    it 'changes the updater and updated time' do
+      updater = User.create :email => 'updater@example.com', :password => 'password longer'
+      sign_in updater
+      @backlog_item = mock_model(BacklogItem, :id => 1)
+      BacklogItem.should_receive(:find).with("#{@backlog_item.id}").and_return @backlog_item
+      @backlog_item.should_receive(:update_attributes).with(
+        'description' => 'something',
+        'updater_id' => updater.id
+      )
+        
+      put :update, {
+        :backlog_item_id => 1,
+        :backlog_item => {
+          :description => 'something'
+        }
+      }
+    end
+  end
+
+  describe '#comment' do
+    it 'changes the updater and updated time' do
+      updater = User.create :email => 'updater@example.com', :password => 'password longer'
+      sign_in updater
+      @backlog_item = BacklogItem.create :name => 'something'
+        
+      post :comment, {
+        :backlog_item_id => 1,
+        :comment => {
+          :comment => 'something'
+        }
+      }
+
+      BacklogItem.find(1).updater_id.should == updater.id
+    end
+  end
 end
