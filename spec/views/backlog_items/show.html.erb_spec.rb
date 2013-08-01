7b0bd021dd48c707a84b9e6ac150fe4fee759c5e
Michael Avila <me@michaelavila.com>
2013-07-31 21:54:36 -0700

    Show empty workers message

--- spec/views/backlog_items/show.html.erb_spec.rb
+++ spec/views/backlog_items/show.html.erb_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 
 describe 'backlog_items/show.html.erb' do
-  it 'uses success colors when item is complete' do
-    assign :backlog_item, BacklogItem.create(:complete => true, :status => '', :creator => User.create, :updater => User.create)
-    
-    render
-    rendered.should have_css('li#status.done')
-    rendered.should have_css('#position.done')
+  before :each do
+    @backlog_item = BacklogItem.create(:complete => true, :status => 'TESTING', :creator => User.create, :updater => User.create)
+    assign :backlog_item, @backlog_item
   end
 
-  it 'uses warning colors when item is not complete' do
-    assign :backlog_item, BacklogItem.create(:complete => false, :status => '', :creator => User.create, :updater => User.create)
-    
-    render
-    rendered.should have_css('li#status.wip')
-    rendered.should have_css('#position.wip')
+  describe 'status' do
+    it 'uses success colors when item is complete' do
+      @backlog_item.update_attribute :complete, true
+      
+      render
+      rendered.should have_css('li#status.done')
+      rendered.should have_css('#position.done')
+    end
+
+    it 'uses warning colors when item is not complete' do
+      @backlog_item.update_attribute :complete, false
+      
+      render
+      rendered.should have_css('li#status.wip')
+      rendered.should have_css('#position.wip')
+    end
+  end
+
+  describe 'people' do
+    it 'shows correct empty message' do
+      render
+      rendered.should have_content("No active people")
+    end
   end
 end
