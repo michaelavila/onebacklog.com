71c93954fefb86b90411e3e0ccc603a7428563cf
Brian Riley <brian@btriley.com>
2013-08-03 09:56:50 -0700

    Change path to bower components

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module OnebacklogCom
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.paths << Rails.root.join("vendor", "assets", "components")
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
   end
 end
