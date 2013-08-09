e607881f00de36a0173b86e74990fac7f8efdb62
Michael Avila <me@michaelavila.com>
2013-08-09 16:53:01 -0700

    Move find checklist item to before_filter

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -1,18 +1,22 @@
 class ChecklistItemsController < ApplicationController
+  before_filter :find_checklist_item, :except => [:destroy, :create]
+
+  def find_checklist_item
+    @checklist_item = ChecklistItem.find params[:checklist_item_id]
+  end
+
   def toggle
-    checklist_item = ChecklistItem.find params[:checklist_item_id]
-    checklist_item.update_attributes :completed => (not checklist_item.completed)
+    @checklist_item.update_attributes :completed => (not @checklist_item.completed)
+    render :nothing => true
+  end
+
+  def update
+    @checklist_item.update_attributes params.require(:checklist_item).permit(:description)
     render :nothing => true
   end
 
   def destroy
     ChecklistItem.delete params[:checklist_item_id]
-    render :nothing => true
-  end
-
-  def update
-    checklist_item = ChecklistItem.find params[:checklist_item_id]
-    checklist_item.update_attributes params.require(:checklist_item).permit(:description)
     render :nothing => true
   end
 
