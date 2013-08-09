24c1b1553c11dd4a16e4d90af7514dfc9fcfd861
Michael Avila <me@michaelavila.com>
2013-08-09 16:34:35 -0700

    Move finding of backlog item to before_filter

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -1,17 +1,16 @@
 class BacklogItemsController < ApplicationController
   respond_to :html, :json
   before_filter :authenticate_user!
+  before_filter :find_backlog_item, :except => [:index]
 
   def index
     @backlog_items = BacklogItem.all
   end
 
   def show
-    @backlog_item = BacklogItem.find params[:backlog_item_id]
   end
 
   def update
-    @backlog_item = BacklogItem.find params[:backlog_item_id]
     params[:backlog_item] = params[:backlog_item].merge :updater_id => current_user.id
     attributes = params.require(:backlog_item).permit :description, :name, :updater_id
     @backlog_item.update_attributes attributes
@@ -19,11 +18,14 @@ class BacklogItemsController < ApplicationController
   end
 
   def comment
-    backlog_item = BacklogItem.find(params[:backlog_item_id])
-    backlog_item.update_attributes :updater_id => current_user.id
-    comment = backlog_item.comments.new params.require(:comment).permit(:comment)
+    @backlog_item.update_attributes :updater_id => current_user.id
+    comment = @backlog_item.comments.new params.require(:comment).permit(:comment)
     comment.user = current_user
     comment.save
     redirect_to backlog_item_path(params[:backlog_item_id])
+  end
+
+  def find_backlog_item
+    @backlog_item = BacklogItem.find params[:backlog_item_id]
   end
 end
